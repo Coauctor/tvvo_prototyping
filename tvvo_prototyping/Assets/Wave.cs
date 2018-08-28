22de4cde3a752ba30b47594085e2c8a9b1a40378
bohdan.sereda <bohdan.sereda@creative-mobile.com>
2018-08-28 22:45:42 +0300

    tek

---
+++ tvvo_prototyping/Assets/Wave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public GameObject Player2;
+    public GameObject WaveObject;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(WaveObject);
+            Debug.Log("Wave!");
+        }
+	}
+
+    //1. если игрок нажал на пробел
+    // - заспавнить объект волна
+    // - волна движется к координатам игрока 2
+
+    //3. когда волна дошла до игрока 2, она возвращается к игроку 1
+    //4. если волна в радиусе Т и игрок 1 не нажал пробел, то уничтожить волну
+    //5. в противном случае — повторить с шага 2
+}
